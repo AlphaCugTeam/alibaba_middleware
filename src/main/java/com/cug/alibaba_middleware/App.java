7d4c649067b93ad24a406b72ec36917770a04108
root <root@master1.(none)>
2016-06-24 00:21:19 +0800

    add file

--- src/main/java/com/cug/alibaba_middleware/App.java
+++ src/main/java/com/cug/alibaba_middleware/App.java
@@ -1,5 +1,8 @@
 package com.cug.alibaba_middleware;
 
+import com.cug.alibaba_middleware.model.OrderMessage;
+import com.cug.alibaba_middleware.model.PaymentMessage;
+
 /**
  * Hello world!
  *
@@ -9,6 +12,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        OrderMessage tbao_orderMessage = OrderMessage.createTbaoMessage();
+        OrderMessage tmall_orderMessage = OrderMessage.createTmallMessage();
+        System.out.println( tbao_orderMessage.toString());
+        System.out.println( tmall_orderMessage.toString());
+
+        PaymentMessage[] tbao_payment_list = PaymentMessage.createPayMentMsg(tbao_orderMessage);
+        PaymentMessage[] tmall_payment_list = PaymentMessage.createPayMentMsg(tmall_orderMessage);
+
+        System.out.println( tbao_payment_list[0].toString());
+        System.out.println( tbao_payment_list[1].toString());
         
+        System.out.println( tmall_payment_list[0].toString());
+        System.out.println( tmall_payment_list[1].toString());
     }
 }
